c88b201d57fe8750c1b54461ca4d425cfeafbce0
israelba <israel.aguiar@gmail.com>
2020-04-29 23:33:10 -0300

    3.17. Mapeando relacionamento com @ManyToOne

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -25,6 +26,7 @@ public class Restaurante {
 
 	private BigDecimal taxaFrete;
 	
+	@ManyToOne
 	private Cozinha cozinha;
 
 }
