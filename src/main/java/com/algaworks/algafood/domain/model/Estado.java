d9ee996d47727bdbe2aa27019acfe4cd01e92e7c
israelba <israel.aguiar@gmail.com>
2020-05-01 10:51:46 -0300

    4.10. Modelando e requisitando um Collection Resource com GET

--- src/main/java/com/algaworks/algafood/domain/model/Estado.java
+++ src/main/java/com/algaworks/algafood/domain/model/Estado.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Estado {
 	
 	@EqualsAndHashCode.Include
