16f8c10cc414188cb97d47737566de02a7196cb5
israelba <israel.aguiar@gmail.com>
2020-05-07 23:25:40 -0300

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -1,9 +1,13 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,4 +23,7 @@ public class FormaPagamento {
 	private Long id;
 	
 	private String descricao;
+	
+	@ManyToMany(mappedBy = "formasPagamento")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,8 +15,11 @@ insert into cidade (id, nome, estado_id) values (2, 'Itambé', 1);
 insert into cidade (id, nome, estado_id) values (3, 'Sorocaba', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Osasco', 2);
 
-insert into forma_pagamento (descricao) values ('À vista');
-insert into forma_pagamento (descricao) values ('À prazo');
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
-insert into permissao (nome, descricao) values ('Administrativa', 'permissão administrativa');
-insert into permissao (nome, descricao) values ('Cliente', 'usuários clientes');
+insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
