9680965cb36e1ad448f96867aa93fbb707927f79
israelba <israel.aguiar@gmail.com>
2020-05-19 17:16:15 -0300

    8.29. Desafio: estendendo o formato do problema

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,7 +39,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String detail = String.format("O recurso %s, que você tentou acessar, é inexistente.",
 				ex.getRequestURL());
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
+				.build();
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
@@ -60,7 +63,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Throwable rootCause = ExceptionUtils.getRootCause(ex);
 		
 		if (rootCause instanceof InvalidFormatException) {
-			return handleInvalidFormatException((InvalidFormatException)rootCause, headers, status, request);
+			return handleInvalidFormat((InvalidFormatException)rootCause, headers, status, request);
 		} else if (rootCause instanceof PropertyBindingException) {
 			return handlePropertyBinding((PropertyBindingException)rootCause, headers, status, request);
 		}
@@ -68,7 +71,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
 		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
+				.build();
 		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(),
 				status, request);
@@ -81,7 +86,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
 		String detail = ex.getMessage();
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.build();
 		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(),
 				status, request);
@@ -111,7 +118,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
 		String detail = ex.getMessage();
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.build();
 		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(),
 				status, request);
@@ -158,10 +167,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.status(status.value())
 				.type(problemType.getUri())
 				.title(problemType.getTitle())
+				.timestamp(LocalDateTime.now())
 				.detail(detail);
 	}
 
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers,
+	private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
 		
 		String path = joinPath(ex.getPath());
@@ -208,7 +218,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 					+ "que é de um tipo inválido. Corrija e informe um valor compatível "
 					+ "com o tipo %s.", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
 			
-			Problem problem = createProblemBuilder(status, problemType, detail).build();
+			Problem problem = createProblemBuilder(status, problemType, detail)
+					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
+					.build();
 			
 			return handleExceptionInternal(ex, problem, new HttpHeaders(),
 					status, request);
