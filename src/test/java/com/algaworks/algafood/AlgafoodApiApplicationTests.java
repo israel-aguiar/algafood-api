81d65c4cffaf2af6b45b20974e30fe25c6af23a5
israelba <israel.aguiar@gmail.com>
2020-05-20 21:57:27 -0300

    10.2. Preparando o projeto para testes de integração

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertFalse(true);
 	}
 
 }
