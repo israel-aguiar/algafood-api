904c176f8630236b8c268c1782683541c2a18c37
israel-aguiar <israel.aguiar@gmail.com>
2020-06-07 13:11:33 -0300

    12.23. Desafio: implementando endpoints de transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -1,10 +1,17 @@
 package com.algaworks.algafood.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum StatusPedido {
 	
-	CRIADO,
-	CONFIRMADO,
-	ENTREGUE,
-	CANCELADO
+	CRIADO("Criado"),
+	CONFIRMADO("Confirmado"),
+	ENTREGUE("Entregue"),
+	CANCELADO("Cancelado");
+	
+	private String descricao;
 
 }
