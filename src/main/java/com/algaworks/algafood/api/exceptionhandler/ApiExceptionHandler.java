f9a05e034eb8c9ab005c6806f9fde2fed69c4f62
israel-aguiar <israel.aguiar@gmail.com>
2020-06-08 12:03:29 -0300

    13.7. Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,6 +41,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+		ex.getBindingResult();
+		return handleValidationInternal(ex, ex.getBindingResult(), status, request);
+	}
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -193,8 +201,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		String detail = "Um ou mais campos estão inválidos."
 				+ " Faça preenchimento correto e tente novamente.";
-		
-//		BindingResult bindingResult = ex.getBindingResult();
 		
 		List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
 				.map(objectError -> {
