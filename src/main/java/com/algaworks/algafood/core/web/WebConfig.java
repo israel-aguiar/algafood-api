53b9fc8107b93e9bb58d08df7dbf1accf94463e5
Israel <israel.aguiar@gmail.com>
2020-08-22 16:28:50 -0300

    17.5. Implementando requisições condicionais com Shallow ETags

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
 //			.allowedOrigins("*")
 //			.maxAge(30);
 		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 
 	
