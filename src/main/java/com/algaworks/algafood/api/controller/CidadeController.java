7ff51e30d7ad1847b894fd622c0bc88becf8fdd6
Israel <israel.aguiar@gmail.com>
2020-08-26 10:33:45 -0300

    18.9. Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -28,6 +28,7 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "Cidades")
 @RestController
@@ -54,14 +55,16 @@ public class CidadeController {
 	
 	@ApiOperation("Busca uma cidade por ID")
 	@GetMapping("/{cidadeId}")
-	public CidadeModel buscar(@PathVariable Long cidadeId) {
+	public CidadeModel buscar(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
 		return cidadeModelAssembler.toModel(cadastroCidade.buscarOuFalhar(cidadeId));
 	}
 	
 	@ApiOperation("Cadastra uma cidade")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeModel adicionar(
+			@ApiParam(name="corpo", value = "Representação de uma nova cidade")
+			@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidade = cidadeInputDisassenbler.toDomainObject(cidadeInput);
 			return cidadeModelAssembler.toModel(cadastroCidade.salvar(cidade));
@@ -73,7 +76,11 @@ public class CidadeController {
 	
 	@ApiOperation("Atualiza uma cidade por ID")
 	@PutMapping("/{cidadeId}")
-	public CidadeModel atualizar(@PathVariable Long cidadeId,
+	public CidadeModel atualizar(
+			@ApiParam(value = "ID de uma cidade", example="1")
+			@PathVariable Long cidadeId,
+			
+			@ApiParam(name="corpo", value = "Representação de uma nova cidade com novos dados")
 			@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
@@ -89,7 +96,9 @@ public class CidadeController {
 	@ApiOperation("Exclui uma cidade por ID")
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cidadeId) {
+	public void remover(
+		@ApiParam(value = "ID de uma cidade", example="1")
+		@PathVariable Long cidadeId) {
 			cadastroCidade.excluir(cidadeId);
 	}
 
