68da49857d03d06b2336bf182d9095cb4927424d
israel-aguiar <israel.aguiar@gmail.com>
2020-09-20 17:43:09 -0300

    18.33. Desafio: descrevendo documentação de endpoints de associação de restaurantes com usuários

---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.UsuarioModel;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteUsuarioResponsavelControllerOpenApi {
+
+	@ApiOperation("Lista os usuários responsáveis associados a restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+	})
+	public List<UsuarioModel> listar(Long restauranteId);
+	
+	@ApiOperation("Desassociação de restaurante com usuário responsável")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+		@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", 
+			response = Problem.class)
+	})
+	public void desassociar(Long restauranteId, Long usuarioId);
+	
+	@ApiOperation("Associação de restaurante com usuário responsável")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+		@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", 
+			response = Problem.class)
+	})
+	public void associar(Long restauranteId, Long usuarioId);
+}
