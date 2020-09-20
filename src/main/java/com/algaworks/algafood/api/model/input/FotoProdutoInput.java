9049dcb5b1f77f4ed4d1284e15e9d361d4ff3d0c
israel-aguiar <israel.aguiar@gmail.com>
2020-09-20 19:13:57 -0300

    18.36. Corrigindo documentação no Swagger UI para upload de arquivos

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -17,8 +17,9 @@ import lombok.Setter;
 @Getter
 public class FotoProdutoInput {
 
-	@ApiModelProperty(value = "Arquivo da foto do produto (máximo 500KB, apenas JPG e PNG)",
-			required = true)
+//	@ApiModelProperty(value = "Arquivo da foto do produto (máximo 500KB, apenas JPG e PNG)",
+//			required = true)
+	@ApiModelProperty(hidden = true)
 	@NotNull
 	@FileSize(max = "500KB")
 	@FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
