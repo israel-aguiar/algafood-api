3a5d57065dd3c8851d23b4fabcadc3d34a00a290
israel-aguiar <israel.aguiar@gmail.com>
2020-10-19 17:28:19 -0300

    19.21. Desafio: refatorando construção e inclusão de links

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -1,24 +1,12 @@
 package com.algaworks.algafood.api.assembler;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.api.controller.CidadeController;
-import com.algaworks.algafood.api.controller.FormaPagamentoController;
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controller.PedidoController;
-import com.algaworks.algafood.api.controller.RestauranteController;
-import com.algaworks.algafood.api.controller.RestauranteProdutoController;
-import com.algaworks.algafood.api.controller.UsuarioController;
 import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 
@@ -27,6 +15,9 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
 	
 	public PedidoModelAssembler() {
 		super(PedidoController.class, PedidoModel.class);
@@ -38,40 +29,18 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 		
 		modelMapper.map(pedido, pedidoModel);
 		
-		TemplateVariables pageVariables = new TemplateVariables(
-				new TemplateVariable("page", VariableType.REQUEST_PARAM),
-				new TemplateVariable("size", VariableType.REQUEST_PARAM),
-				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		pedidoModel.add(algaLinks.linkToPedidos());
 		
-		TemplateVariables filtroVariables = new TemplateVariables(
-				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM)
-				);
+		pedidoModel.getRestaurante().add(algaLinks.linkToRestaurante(pedido.getRestaurante().getId()));
 		
-		String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+		pedidoModel.getCliente().add(algaLinks.linkToUsuario(pedido.getCliente().getId()));
 		
-		pedidoModel.add(new Link(UriTemplate.of(pedidosUrl, pageVariables.concat(filtroVariables)), "pedidos"));
-		
-		pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
-				.buscar(pedido.getRestaurante().getId())).withSelfRel());
-		
-		pedidoModel.getCliente().add(linkTo(methodOn(UsuarioController.class)
-				.buscar(pedido.getCliente().getId())).withSelfRel());
-		
-		// Passamos null no segundo argumento, porque é indiferente para a
-		// construção da URL do recurso de forma de pagamento
-		pedidoModel.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
-				.buscar(pedido.getFormaPagamento().getId(), null)).withSelfRel());
+		pedidoModel.getFormaPagamento().add(algaLinks.linkToFormaPagamento(pedido.getFormaPagamento().getId()));
 
-		pedidoModel.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
-				.buscar(pedido.getEnderecoEntrega().getCidade().getId())).withSelfRel());
+		pedidoModel.getEnderecoEntrega().getCidade().add(algaLinks.linkToCidade(pedido.getEnderecoEntrega().getCidade().getId()));
 
 		pedidoModel.getItens().forEach(item -> {
-			item.add(linkTo(methodOn(RestauranteProdutoController.class)
-				.buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId()))
-				.withRel("produto"));
+			item.add(algaLinks.linkToProduto(pedido.getRestaurante().getId(), item.getProdutoId(), "produtos"));
 		});
 		
 		return pedidoModel;
