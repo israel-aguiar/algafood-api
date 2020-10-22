22b0eb4ebad4e510135b32e78a3ceb1c85cd19c8
israel-aguiar <israel.aguiar@gmail.com>
2020-10-22 16:15:56 -0300

    19.33. Desafio: adicionando hypermedia nos recursos de grupos

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -16,6 +16,8 @@ import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.controller.EstadoController;
 import com.algaworks.algafood.api.controller.FluxoPedidoController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
+import com.algaworks.algafood.api.controller.GrupoController;
+import com.algaworks.algafood.api.controller.GrupoPermissaoController;
 import com.algaworks.algafood.api.controller.PedidoController;
 import com.algaworks.algafood.api.controller.RestauranteController;
 import com.algaworks.algafood.api.controller.RestauranteFormaPagamentoController;
@@ -161,6 +163,21 @@ public class AlgaLinks {
 		return linkToGruposUsuario(usuarioId, IanaLinkRelations.SELF.value());
 	}
 	
+	public Link linkToGrupos(String rel) {
+		return linkTo(methodOn(GrupoController.class).listar()).withRel(rel);
+	}
+	
+	public Link linkToGrupos() {
+		return linkToGrupos(IanaLinkRelations.SELF.value());
+	}
+
+	public Link linkToGrupoPermissoes(Long grupoId, String rel) {
+		return linkTo(methodOn(GrupoPermissaoController.class).listar(grupoId)).withRel(rel);
+	}
+
+	public Link linkToGrupoPermissoes(Long grupoId) {
+		return linkToGrupoPermissoes(grupoId, IanaLinkRelations.SELF.value());
+	}
 	
 	public Link linkToResponsaveisRestaurante(Long restauranteId, String rel) {
 		return linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
