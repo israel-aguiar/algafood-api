6cc7e23b47a2732323208aa37d27d0c1e9ba5302
israel-aguiar <israel.aguiar@gmail.com>
2020-10-23 14:33:15 -0300

    19.37. Desafio: implementando endpoint com links de recursos de estatísticas

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -6,14 +6,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
 import org.springframework.hateoas.TemplateVariables;
 import org.springframework.hateoas.UriTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.hateoas.TemplateVariable.VariableType;
 
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.controller.EstadoController;
+import com.algaworks.algafood.api.controller.EstatisticasController;
 import com.algaworks.algafood.api.controller.FluxoPedidoController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.GrupoController;
@@ -310,6 +311,23 @@ public class AlgaLinks {
 	
 	public Link linkToCozinhas() {
 		return linkTo(CozinhaController.class).withRel(IanaLinkRelations.SELF.value());
+	}
+	
+	public Link linkToEstatisticas(String rel) {
+		return linkTo(EstatisticasController.class).withRel(rel);
+	}
+
+	public Link linkToEstatisticasVendasDiarias(String rel) {
+		TemplateVariables filtroVariables = new TemplateVariables(
+				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM),
+				new TemplateVariable("timeOffset", VariableType.REQUEST_PARAM));
+
+		String pedidosUrl = linkTo(methodOn(EstatisticasController.class).consultarVendasDiarias(null, null)).toUri()
+				.toString();
+
+		return new Link(UriTemplate.of(pedidosUrl, filtroVariables), rel);
 	}
 	
 }
