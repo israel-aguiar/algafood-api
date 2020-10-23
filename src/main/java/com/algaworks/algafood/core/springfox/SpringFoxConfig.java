ecaa562b4eef83103fd6033339c52c8185c4d863
israel-aguiar <israel.aguiar@gmail.com>
2020-10-23 18:46:18 -0300

    19.39. Corrigindo as propriedades de links na documentação

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -24,6 +25,7 @@ import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PedidosResumoModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
@@ -73,11 +75,15 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 			.ignoredParameterTypes(ServletWebRequest.class,
 					URL.class, URI.class, URLStreamHandler.class, Resource.class,
 					File.class, InputStream.class)
+			
 			.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+			.directModelSubstitute(Links.class, LinksModelOpenApi.class)
+			
 			.alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, CozinhaModel.class),
 					CozinhasModelOpenApi.class))
 			.alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, PedidoResumoModel.class),
 					PedidosResumoModelOpenApi.class))
+			
 			.apiInfo(apiInfo())
 			.tags(
 				new Tag("Cidades", "Gerencia as cidades"),
