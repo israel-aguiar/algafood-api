4ec02f819be5c5ac23d6f1e3d447be06c3329612
israel-aguiar <israel.aguiar@gmail.com>
2020-10-29 21:06:32 -0300

    21.1. Introdução ao Logback e SLF4J

--- src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
@@ -30,10 +30,13 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/v1/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController implements CozinhaControllerOpenApi {
-
+	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
@@ -51,6 +54,8 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	
 	@GetMapping
 	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
+		log.info("Consultado cozinhas com páginas de {} registros...", pageable.getPageSize());
+		
 		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 		
 		PagedModel<CozinhaModel> cozinhasPagedModel = pagedResourcesAssembler
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -2,19 +2,17 @@ package com.algaworks.algafood.core.web;
 
 import javax.servlet.Filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
-	@Autowired
-	private ApiRetirementHandler apiRetirementHandler;
+//	@Autowired
+//	private ApiRetirementHandler apiRetirementHandler;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -25,10 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
 		WebMvcConfigurer.super.addCorsMappings(registry);
 	}
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(apiRetirementHandler);
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(apiRetirementHandler);
+//	}
 	
 	@Bean
 	public Filter shallowEtagHeaderFilter() {
